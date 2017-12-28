330bcbde3a5920371a2f91eebecdeea3470981d2
Dmitry Trifonov <slonegg@gmail.com>
2017-12-28 10:51:04 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='deep-ctrl',
+      version='',
+      description='',
+      author='Dmitry Trifonov & Natalia Trifonova',
+      author_email='')
